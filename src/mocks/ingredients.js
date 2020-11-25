95a9a3ee4087881889f797af378362276713474f
Henrique <hbussularr@gmail.com>
2020-11-25 12:22:17 -0300

    Adiciona dados de ingredientes

---
+++ src/mocks/ingredients.js
@@ -0,0 +1,64 @@
+export const saladIngredients = [
+  [
+    {
+      name: "Alface",
+      picture: ""
+    },
+    {
+      name: "Tomate",
+      picture: ""
+    },
+    {
+      name: "Cebola",
+      picture: ""
+    }
+  ],
+  [
+    {
+      name: "Picles",
+      picture: ""
+    },
+    {
+      name: "Rucula",
+      picture: ""
+    },
+    {
+      name: "Champignon",
+      picture: ""
+    }
+  ]
+];
+
+export const breadIngredients = [
+  [
+    {
+      name: "Pão Tradicional",
+      picture: ""
+    },
+    {
+      name: "Pão Italiano",
+      picture: ""
+    },
+    {
+      name: "Pão Integral",
+      picture: ""
+    }
+  ]
+];
+
+export const meatIngredients = [
+  [
+    {
+      name: "Bem Passada",
+      picture: ""
+    },
+    {
+      name: "Ao Ponto",
+      picture: ""
+    },
+    {
+      name: "Mal Passada",
+      picture: ""
+    }
+  ]
+];

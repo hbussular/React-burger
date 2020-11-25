d9a73f5410661654fb05a285210d4d1792fb153d
Henrique <hbussularr@gmail.com>
2020-11-25 12:04:11 -0300

    Adiciona uma nova propriedade na tela de Startup

--- src/pages/Startup/Startup.js
+++ src/pages/Startup/Startup.js
@@ -1,23 +1,25 @@
-import React from "react"
-import { Image } from "react-bootstrap"
+import React from "react";
+import { Image } from "react-bootstrap";
 
-import Layout from "../../components/Layout/Layout"
-import Button from "../../components/Button/PrimaryButton"
+import Layout from "../../components/Layout/Layout";
+import PrimaryButton from "../../components/Button/PrimaryButton";
 
-import logo from "../../assets/images/logo.svg"
-import "./Startup.css"
+import logo from "../../assets/images/logo.svg";
+import "./Startup.css";
 
-const Startup = () => (
+const Startup = ({ onNext }) => (
   <Layout>
     <div className="startup">
       <div className="startup__image-container">
         <Image src={logo} className="startup__logo" fluid />
       </div>
       <div className="startup__button-container">
-        <Button block>Realizar Pedido</Button>
+        <PrimaryButton block onClick={onNext}>
+          Realizar Pedido
+        </PrimaryButton>
       </div>
     </div>
   </Layout>
-)
+);
 
-export default Startup
+export default Startup;

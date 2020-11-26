b23eaa9867ee11cb90a6d9628350d9f6831ad44a
Henrique <hbussularr@gmail.com>
2020-11-26 11:06:34 -0300

    Remove o WebVitails

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-// arquivo de inicialização do projeto que pegará todos os nós e renderizar dentro da div root do index.html
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import AppRouter from "./AppRouter";
-// import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,8 +9,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();

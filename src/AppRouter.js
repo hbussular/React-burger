e826165f5100e49337f1e2fb05e47ed21d959aac
Henrique <hbussularr@gmail.com>
2020-11-26 11:05:07 -0300

    Inclui a nova store e provider no AppRouiter

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,67 +1,39 @@
-import React, { useState } from "react";
+import React from "react";
+
 import Startup from "./pages/Startup/Startup";
 import ChoicesBread from "./pages/Choices/ChoicesBread";
 import ChoicesMeat from "./pages/Choices/ChoicesMeat";
-import ChoicesSalad from "./pages/Choices/ChoicesSalad";
+import ChoicesSouce from "./pages/Choices/ChoicesSouce";
+import ChoicesCheese from "./pages/Choices/ChoicesCheese";
 import Destination from "./pages/Destination/Destination";
+import Confirmation from "./pages/Confirmation/Confirmation";
 
-const INITIAL_STATE = {
-  ingredients: [],
-  address: null,
-  weather: null
-};
+import { StateProvider } from "./store.js";
 
-export const AppContext = React.createContext(INITIAL_STATE);
-
-const AppRouter = () => {
-  const [currentPage, setCurrentPage] = useState("destination");
-
-  const [ingredients, setIngredients] = React.useState(
-    INITIAL_STATE.ingredients
-  );
-  const [address, setAddress] = React.useState(INITIAL_STATE.address);
-  const [weather, setWeather] = React.useState(INITIAL_STATE.weather);
-
-  const setContext = (data, path) => {
-    switch (path) {
-      case "ingredients":
-        setIngredients([...ingredients, data]);
-        break;
-      case "address":
-        setAddress(data);
-        break;
-      case "weather":
-        setWeather(data);
-        break;
-      default:
-        break;
-    }
-  };
-
-  return (
-    <AppContext.Provider
-      value={[{ address, weather, ingredients }, setContext]}
-    >
-      {(() => {
-        switch (currentPage) {
-          case "startup":
-            return <Startup onNext={() => setCurrentPage("bread")} />;
-          case "bread":
-            return <ChoicesBread setCurrentPage={setCurrentPage} />;
-          case "meat":
-            return <ChoicesMeat setCurrentPage={setCurrentPage} />;
-          case "salad":
-            return <ChoicesSalad setCurrentPage={setCurrentPage} />;
-          case "destination":
-            return <Destination setCurrentPage={setCurrentPage} />;
-          case "confirmation":
-            return <Destination setCurrentPage={setCurrentPage} />;
-          default:
-            return null;
-        }
-      })()}
-    </AppContext.Provider>
-  );
-};
+const AppRouter = () => (
+  <StateProvider>
+    {state => {
+      console.log(state);
+      switch (state.currentPage) {
+        case "startup":
+          return <Startup />;
+        case "bread":
+          return <ChoicesBread />;
+        case "meat":
+          return <ChoicesMeat />;
+        case "souce":
+          return <ChoicesSouce />;
+        case "cheese":
+          return <ChoicesCheese />;
+        case "destination":
+          return <Destination />;
+        case "confirmation":
+          return <Confirmation />;
+        default:
+          return null;
+      }
+    }}
+  </StateProvider>
+);
 
 export default AppRouter;

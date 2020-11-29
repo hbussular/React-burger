7ccb5b9ccb4bf8894cd2ac7ce05172b1b234c9d0
Henrique <hbussularr@gmail.com>
2020-11-29 11:31:17 -0300

    Simplifica as estruturas dos mocks

--- src/mocks/ingredients.js
+++ src/mocks/ingredients.js
@@ -6,81 +6,73 @@ import bread from "../assets/images/bread.png";
 import cheese from "../assets/images/cheese.png";
 
 export const souceIngredients = [
-  [
-    {
-      name: "Maionese",
-      shortName: "Maionese",
-      picture: mayonnaise
-    },
-    {
-      name: "Mostarda",
-      shortName: "Mostarda",
-      picture: mostard
-    },
-    {
-      name: "Ketchup",
-      shortName: "Ketchup",
-      picture: ketchup
-    }
-  ]
+  {
+    name: "Maionese",
+    shortName: "Maionese",
+    picture: mayonnaise
+  },
+  {
+    name: "Mostarda",
+    shortName: "Mostarda",
+    picture: mostard
+  },
+  {
+    name: "Ketchup",
+    shortName: "Ketchup",
+    picture: ketchup
+  }
 ];
 
 export const breadIngredients = [
-  [
-    {
-      name: "Pão Tradicional",
-      shortName: "Tradicional",
-      picture: bread
-    },
-    {
-      name: "Pão Italiano",
-      shortName: "Italiano",
-      picture: bread
-    },
-    {
-      name: "Pão Integral",
-      shortName: "Integral",
-      picture: bread
-    }
-  ]
+  {
+    name: "Pão Tradicional",
+    shortName: "Tradicional",
+    picture: bread
+  },
+  {
+    name: "Pão Italiano",
+    shortName: "Italiano",
+    picture: bread
+  },
+  {
+    name: "Pão Integral",
+    shortName: "Integral",
+    picture: bread
+  }
 ];
 
 export const meatIngredients = [
-  [
-    {
-      name: "Carne Bem Passada",
-      shortName: "Bem Passada",
-      picture: beef
-    },
-    {
-      name: "Carne Ao Ponto",
-      shortName: "Ao Ponto",
-      picture: beef
-    },
-    {
-      name: "Carne Mal Passada",
-      shortName: "Mal Passada",
-      picture: beef
-    }
-  ]
+  {
+    name: "Carne Bem Passada",
+    shortName: "Bem Passada",
+    picture: beef
+  },
+  {
+    name: "Carne Ao Ponto",
+    shortName: "Ao Ponto",
+    picture: beef
+  },
+  {
+    name: "Carne Mal Passada",
+    shortName: "Mal Passada",
+    picture: beef
+  }
 ];
 
 export const cheeseIngredients = [
-  [
-    {
-      name: "Queijo Cheddar",
-      shortName: "Queijo Cheddar",
-      picture: cheese
-    },
-    {
-      name: "Queijo Prato",
-      shortName: "Queijo Prato",
-      picture: cheese
-    },
-    {
-      name: "Queijo Branco",
-      shortName: "Queijo Branco",
-      picture: cheese
-    }
-  ]
+  {
+    name: "Queijo Cheddar",
+    shortName: "Queijo Cheddar",
+    picture: cheese
+  },
+  {
+    name: "Queijo Prato",
+    shortName: "Queijo Prato",
+    picture: cheese
+  },
+  {
+    name: "Queijo Branco",
+    shortName: "Queijo Branco",
+    picture: cheese
+  }
 ];

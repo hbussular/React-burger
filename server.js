27b57576cc106084ddf5bcd4690105e789fde6b7
Henrique <hbussularr@gmail.com>
2020-11-29 12:16:28 -0300

    Corrige problema de CORS

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const favicon = require("express-favicon");
 const path = require("path");
+const cors = require("cors");
+
 const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(favicon(__dirname + "/build/favicon.ico"));
-
+app.use(cors());
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
 
